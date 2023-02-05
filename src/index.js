7e14940d2e9c42dafe2a55e3b37da27f33507304
Serj <serjml@list.ru>
2023-02-05 20:50:41 +0300

    add app layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('first');
+const buttonsValue = [
+  'C',
+  '√',
+  '%',
+  '/',
+  '7',
+  '8',
+  '9',
+  '×',
+  '4',
+  '5',
+  '6',
+  '-',
+  '1',
+  '2',
+  '3',
+  '+',
+  '00',
+  '0',
+  ',',
+  '=',
+];
+
+const buttons = document.querySelector('#buttons');
+
+buttonsValue.forEach((value) => {
+  const button = document.createElement('div');
+  if (value === '=') {
+    button.classList.add('button_equal');
+  }
+
+  button.classList.add('button');
+  button.innerText = value;
+  buttons.append(button);
+});
